03c4c955393db9d28a13a61d4b777263901eaaaa
steveraaen <sraaen@gmail.com>
2017-02-22 19:57:27 -0500

    Validation added. Getting into tricky part.  Good rool-back point.

--- README.md
+++ README.md
@@ -10,4 +10,16 @@
 				5- Check to see if there in sufficient quantity
 				6- Decrement quantity.
 
-  	
+Leash, Pets, 9.99, 10
+Shovel, Gardening, 15.95, 30
+Teapot, Home, 6.50, 100
+Clock, Home, 12, 30
+Seeds, Gardening, 1, 1000
+Catnip, Pets, 20, 10
+Greenies, Pets, 15, 100
+Towel, Home, 12.50, 20
+Pillow, Home, 15, 20
+Ferilizer, Gardening, 5, 50
+Fish Food, Pets, 5, 20
+Pot, Gardening, 1.50, 50
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 // Declare empty array that will list products.  Connect to db.
-var prodList = [];
+var prod = [];
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -22,18 +22,34 @@ var getData = function() {
     }
     connection.query('SELECT * FROM products', function(err, res) {
         if (err) throw err;
-        var newArr = res.map(getProducts)
-        console.log(newArr);
-
+        var newArr = res.map(getProducts);
+        console.log(newArr)
         inquirer.prompt([{
             name: "item",
             type: "rawlist",
             message: "What item would you like to buy?",
             choices: newArr
-        }]).then(function(answer){
-        	console.log(answer)
-        })
+        }, {
+            name: "qty",
+            type: "input",
+            message: "How many would you like to buy?",
+            validate: function(value) {
+                connection.query('select inventory  from products where id = 3', function(err, res) {
+                    if (err) throw err;
+                })
+                if (res[0].inventory >= value) {
+                    return true;
+                } else {
+                    return 'Sorry we don\'t have that many in stock.  Try ordering fewer of these';
+                }
+            }
+        }]).then(function(answer) {
+            prod = answer.item;
+            var splProd = prod.split(' ');
+            var itemId = splProd[0];
+            var qty = answer.qty;
 
+        })
 
     });
 
