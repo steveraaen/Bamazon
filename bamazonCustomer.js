6770d53ca5d70810f21216839dab6404ecb21480
steveraaen <sraaen@gmail.com>
2017-02-27 11:57:21 -0500

    finished assignment and added video

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,22 +8,24 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "felisa",
-    database: "Bamazon"
+    database: "Bamazon",
+    multipleStatements: true
 });
 connection.connect(function(err) {
     if (err) throw err;
     console.log('connected as  ' + connection.threadId);
     getData()
 })
+// ----------------------------------------------------------------------
 var getData = function() {
     function getProducts(item, index) {
-        var details = [item.id, item.product_name, item.price].join(" ");
+        var details = [item.id , item.product_name, item.price].join(" ");
         return details;
     }
     connection.query('SELECT * FROM products', function(err, res) {
         if (err) throw err;
         var newArr = res.map(getProducts);
-        console.log(newArr)
+        console.log('Check out our low, low prices!')
         inquirer.prompt([{
             name: "item",
             type: "rawlist",
@@ -34,7 +36,9 @@ var getData = function() {
             type: "input",
             message: "How many would you like to buy?",
             validate: function(value) {
-                connection.query('select inventory  from products where id = 3', function(err, res) {
+                connection.query('select inventory  from products where ?',[{
+                    id: 'qty'
+                }], function(err, res) {
                     if (err) throw err;
                 })
                 if (res[0].inventory >= value) {
@@ -46,11 +50,35 @@ var getData = function() {
         }]).then(function(answer) {
             prod = answer.item;
             var splProd = prod.split(' ');
-            var itemId = splProd[0];
             var qty = answer.qty;
+            var itemId = splProd[0];
+            var prod = splProd[1]
+            var price = splProd[2];
+            var taxRate = .0875;
+            var subTotal = parseInt(qty) * parseFloat(price)
+            var withTax = subTotal * taxRate;
+//--------------------------------------------------------------------------
+// Check variables
+console.log('You have ordered : ' +  qty + ' of our product = ' + prod + ' at $' + price + ' each.' )
+console.log('TOTAL COST:');
+console.log('_____________________');
+console.log('Products : $' + parseFloat(subTotal).toFixed(2));
+console.log('Taxes    : $' + parseFloat(withTax).toFixed(2));
+console.log('---------------------');
+console.log('Total    : $' + parseFloat(subTotal + withTax).toFixed(2));
 
-        })
+// ------------------------------------------------------------------------- 
+              connection.query("UPDATE products SET ? WHERE ? ", [{
+                inventory: parseInt(res[0].inventory - qty)
+              },
+              {
+                id: itemId
+              }]);
+              process.exit();
+        });
+        
 
     });
 
 }
+
